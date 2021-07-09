7c678cc5bf87955eaef9c49877c2241dc9137535
Ethan M <53354219+devethanm@users.noreply.github.com>
2021-07-09 16:41:47 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+#################################
+# Artix linux installation script
+#################################
+# NOTES:
+# This script assumes your installation disk is /dev/sda, if you want to install to a different disk change the $disk variable accordingly
+#
+#
+#
 
