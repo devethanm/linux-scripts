a31fb5f95b1d3e3d0c7e37415c1b986ce0cfc2fb
Ethan M <53354219+devethanm@users.noreply.github.com>
2021-07-09 16:50:18 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,10 @@
 #################################
 # Artix linux installation script
 #################################
-# NOTES:
+# IMPORTATN NOTES:
+# 
 # This script assumes your installation disk is /dev/sda, if you want to install to a different disk change the $disk variable accordingly
 #
-#
-#
+# This script will make a 1GB boot, and 30GB root partition. The 3rd partition will use the rest of your remaining diskspace as a home partition.
+# If you wish to change any of this, see the "PARTITIONING" section
 
