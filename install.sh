5a1eb1a102047c0ca3507885f5b06954c7eaf067
Ethan M <53354219+devethanm@users.noreply.github.com>
2021-07-09 16:51:18 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,10 @@
 # IMPORTATN NOTES:
 # 
 # This script assumes your installation disk is /dev/sda, if you want to install to a different disk change the $disk variable accordingly
+# It also assumes that /dev/sda is an empty disk. If it is not, make sure to delete all existing partitions before running this script.
 #
 # This script will make a 1GB boot, and 30GB root partition. The 3rd partition will use the rest of your remaining diskspace as a home partition.
 # If you wish to change any of this, see the "PARTITIONING" section
+#
+#
 
